203d710d80d404a13b00abec4ffcfc83a3e92ce5
Maurus Kühne <mauruskuehne@gmail.com>
2014-10-26 18:49:10 +0100

    implemented all rotate opcodes

--- GBEmu/GBEmu.Emulation/Instructions/RotateAndShift.cs
+++ GBEmu/GBEmu.Emulation/Instructions/RotateAndShift.cs
@@ -83,7 +83,313 @@ namespace GBEmu.Emulation
 
 			case 0xCB:
 
-				//this is getting stupid... any way to unify this code????
+				var nextOpcode = memoryAccess.ReadByteAtAddress (registers.GetPC (peek: true));
+
+				switch (nextOpcode) {
+
+				//RLC n
+
+				case 0x07:
+					bit7Val = (byte)(registers.A & 0xFF);
+					newFirstBit = (byte)(bit7Val == 0 ? 0 : 1);
+					newVal = (byte)((registers.A << 1) + newFirstBit);
+					registers.A = newVal;
+					registers.SetFlagForUnsignedByteOperation (Operation.RLCA, registers.A);
+					res.ClockCycles = 8;
+					break;
+				case 0x00:
+					bit7Val = (byte)(registers.B & 0xFF);
+					newFirstBit = (byte)(bit7Val == 0 ? 0 : 1);
+					newVal = (byte)((registers.B << 1) + newFirstBit);
+					registers.B = newVal;
+					registers.SetFlagForUnsignedByteOperation (Operation.RLCA, registers.B);
+					res.ClockCycles = 8;
+					break;
+				case 0x01:
+					bit7Val = (byte)(registers.C & 0xFF);
+					newFirstBit = (byte)(bit7Val == 0 ? 0 : 1);
+					newVal = (byte)((registers.C << 1) + newFirstBit);
+					registers.C = newVal;
+					registers.SetFlagForUnsignedByteOperation (Operation.RLCA, registers.C);
+					res.ClockCycles = 8;
+					break;
+				case 0x02:
+					bit7Val = (byte)(registers.D & 0xFF);
+					newFirstBit = (byte)(bit7Val == 0 ? 0 : 1);
+					newVal = (byte)((registers.D << 1) + newFirstBit);
+					registers.D = newVal;
+					registers.SetFlagForUnsignedByteOperation (Operation.RLCA, registers.D);
+					res.ClockCycles = 8;
+					break;
+				case 0x03:
+					bit7Val = (byte)(registers.E & 0xFF);
+					newFirstBit = (byte)(bit7Val == 0 ? 0 : 1);
+					newVal = (byte)((registers.E << 1) + newFirstBit);
+					registers.E = newVal;
+					registers.SetFlagForUnsignedByteOperation (Operation.RLCA, registers.E);
+					res.ClockCycles = 8;
+					break;
+				case 0x04:
+					bit7Val = (byte)(registers.H & 0xFF);
+					newFirstBit = (byte)(bit7Val == 0 ? 0 : 1);
+					newVal = (byte)((registers.H << 1) + newFirstBit);
+					registers.H = newVal;
+					registers.SetFlagForUnsignedByteOperation (Operation.RLCA, registers.H);
+					res.ClockCycles = 8;
+					break;
+				case 0x05:
+					bit7Val = (byte)(registers.L & 0xFF);
+					newFirstBit = (byte)(bit7Val == 0 ? 0 : 1);
+					newVal = (byte)((registers.L << 1) + newFirstBit);
+					registers.L = newVal;
+					registers.SetFlagForUnsignedByteOperation (Operation.RLCA, registers.L);
+					res.ClockCycles = 8;
+					break;
+				case 0x06:
+					byte oldVal = memoryAccess.ReadByteAtAddress (registers.HL);
+					bit7Val = (byte)(oldVal & 0xFF);
+					newFirstBit = (byte)(bit7Val == 0 ? 0 : 1);
+					newVal = (byte)((oldVal << 1) + newFirstBit);
+					memoryAccess.WriteAtAddress (registers.HL, newVal);
+					registers.SetFlagForUnsignedByteOperation (Operation.RLCA, newVal);
+					res.ClockCycles = 16;
+					break;
+
+					//RL n
+
+				case 0x17:
+					oldVal = registers.A;
+					bit7Val = (byte)(oldVal & 0xFF);
+					newFirstBit = (byte)( bit7Val == 0 ? 0 : 1);
+					newVal = (byte)((oldVal << 1) + registers.GetFlag(Flags.C));
+					registers.A = newVal;
+					registers.SetFlagForUnsignedByteOperation (Operation.RLA_OR_RRA, newVal, newFirstBit);
+					res.ClockCycles = 8;
+					break;
+				case 0x10:
+					oldVal = registers.B;
+					bit7Val = (byte)(oldVal & 0xFF);
+					newFirstBit = (byte)( bit7Val == 0 ? 0 : 1);
+					newVal = (byte)((oldVal << 1) + registers.GetFlag(Flags.C));
+					registers.B = newVal;
+					registers.SetFlagForUnsignedByteOperation (Operation.RLA_OR_RRA, newVal, newFirstBit);
+					res.ClockCycles = 8;
+					break;
+				case 0x11:
+					oldVal = registers.C;
+					bit7Val = (byte)(oldVal & 0xFF);
+					newFirstBit = (byte)( bit7Val == 0 ? 0 : 1);
+					newVal = (byte)((oldVal << 1) + registers.GetFlag(Flags.C));
+					registers.C = newVal;
+					registers.SetFlagForUnsignedByteOperation (Operation.RLA_OR_RRA, newVal, newFirstBit);
+					res.ClockCycles = 8;
+					break;
+				case 0x12:
+					oldVal = registers.D;
+					bit7Val = (byte)(oldVal & 0xFF);
+					newFirstBit = (byte)( bit7Val == 0 ? 0 : 1);
+					newVal = (byte)((oldVal << 1) + registers.GetFlag(Flags.C));
+					registers.D = newVal;
+					registers.SetFlagForUnsignedByteOperation (Operation.RLA_OR_RRA, newVal, newFirstBit);
+					res.ClockCycles = 8;
+					break;
+				case 0x13:
+					oldVal = registers.E;
+					bit7Val = (byte)(oldVal & 0xFF);
+					newFirstBit = (byte)( bit7Val == 0 ? 0 : 1);
+					newVal = (byte)((oldVal << 1) + registers.GetFlag(Flags.C));
+					registers.E = newVal;
+					registers.SetFlagForUnsignedByteOperation (Operation.RLA_OR_RRA, newVal, newFirstBit);
+					res.ClockCycles = 8;
+					break;
+				case 0x14:
+					oldVal = registers.H;
+					bit7Val = (byte)(oldVal & 0xFF);
+					newFirstBit = (byte)( bit7Val == 0 ? 0 : 1);
+					newVal = (byte)((oldVal << 1) + registers.GetFlag(Flags.C));
+					registers.H = newVal;
+					registers.SetFlagForUnsignedByteOperation (Operation.RLA_OR_RRA, newVal, newFirstBit);
+					res.ClockCycles = 8;
+					break;
+				case 0x15:
+					oldVal = registers.L;
+					bit7Val = (byte)(oldVal & 0xFF);
+					newFirstBit = (byte)( bit7Val == 0 ? 0 : 1);
+					newVal = (byte)((oldVal << 1) + registers.GetFlag(Flags.C));
+					registers.L = newVal;
+					registers.SetFlagForUnsignedByteOperation (Operation.RLA_OR_RRA, newVal, newFirstBit);
+					res.ClockCycles = 8;
+					break;
+				case 0x16:
+					oldVal = memoryAccess.ReadByteAtAddress (registers.HL);
+					bit7Val = (byte)(oldVal & 0xFF);
+					newFirstBit = (byte)(bit7Val == 0 ? 0 : 1);
+					newVal = (byte)((oldVal << 1) + registers.GetFlag (Flags.C));
+					memoryAccess.WriteAtAddress (registers.HL, newVal);
+					registers.SetFlagForUnsignedByteOperation (Operation.RLA_OR_RRA, newVal, newFirstBit);
+					res.ClockCycles = 8;
+					break;
+
+					//RRC n
+
+				case 0x0F:
+					oldVal = registers.A;
+					bit0Val = (byte)(oldVal & 0x01);
+					newLastBit = (byte)(bit0Val == 0 ? 0 : 0xFF);
+					newVal = (byte)((oldVal >> 1) + newLastBit);
+					registers.A = newVal;
+					registers.SetFlagForUnsignedByteOperation (Operation.RRCA, newVal);
+					res.ClockCycles = 8;
+					break;
+				case 0x08:
+					oldVal = registers.B;
+					bit0Val = (byte)(oldVal & 0x01);
+					newLastBit = (byte)(bit0Val == 0 ? 0 : 0xFF);
+					newVal = (byte)((oldVal >> 1) + newLastBit);
+					registers.B = newVal;
+					registers.SetFlagForUnsignedByteOperation (Operation.RRCA, newVal);
+					res.ClockCycles = 8;
+					break;
+				case 0x09:
+					oldVal = registers.C;
+					bit0Val = (byte)(oldVal & 0x01);
+					newLastBit = (byte)(bit0Val == 0 ? 0 : 0xFF);
+					newVal = (byte)((oldVal >> 1) + newLastBit);
+					registers.C = newVal;
+					registers.SetFlagForUnsignedByteOperation (Operation.RRCA, newVal);
+					res.ClockCycles = 8;
+					break;
+				case 0x0A:
+					oldVal = registers.D;
+					bit0Val = (byte)(oldVal & 0x01);
+					newLastBit = (byte)(bit0Val == 0 ? 0 : 0xFF);
+					newVal = (byte)((oldVal >> 1) + newLastBit);
+					registers.D = newVal;
+					registers.SetFlagForUnsignedByteOperation (Operation.RRCA, newVal);
+					res.ClockCycles = 8;
+					break;
+				case 0x0B:
+					oldVal = registers.E;
+					bit0Val = (byte)(oldVal & 0x01);
+					newLastBit = (byte)(bit0Val == 0 ? 0 : 0xFF);
+					newVal = (byte)((oldVal >> 1) + newLastBit);
+					registers.E = newVal;
+					registers.SetFlagForUnsignedByteOperation (Operation.RRCA, newVal);
+					res.ClockCycles = 8;
+					break;
+				case 0x0C:
+					oldVal = registers.H;
+					bit0Val = (byte)(oldVal & 0x01);
+					newLastBit = (byte)(bit0Val == 0 ? 0 : 0xFF);
+					newVal = (byte)((oldVal >> 1) + newLastBit);
+					registers.H = newVal;
+					registers.SetFlagForUnsignedByteOperation (Operation.RRCA, newVal);
+					res.ClockCycles = 8;
+					break;
+				case 0x0D:
+					oldVal = registers.L;
+					bit0Val = (byte)(oldVal & 0x01);
+					newLastBit = (byte)(bit0Val == 0 ? 0 : 0xFF);
+					newVal = (byte)((oldVal >> 1) + newLastBit);
+					registers.L = newVal;
+					registers.SetFlagForUnsignedByteOperation (Operation.RRCA, newVal);
+					res.ClockCycles = 8;
+					break;
+				case 0x0E:
+					oldVal = memoryAccess.ReadByteAtAddress (registers.HL);
+					bit0Val = (byte)(oldVal & 0x01);
+					newLastBit = (byte)(bit0Val == 0 ? 0 : 0xFF);
+					newVal = (byte)((oldVal >> 1) + newLastBit);
+					memoryAccess.WriteAtAddress (registers.HL, newVal);
+					registers.SetFlagForUnsignedByteOperation (Operation.RRCA, newVal);
+					res.ClockCycles = 16;
+					break;
+
+
+					//RR n
+
+
+				case 0x1F:
+					oldVal = registers.A;
+					bit0Val = (byte)(oldVal & 0x01);
+					newLastBit = (byte)( bit0Val == 0 ? 0 : 1);
+					newVal = (byte)((oldVal >> 1) + (registers.GetFlag(Flags.C) == 1 ? 0xFF : 0));
+					registers.A = newVal;
+					registers.SetFlagForUnsignedByteOperation (Operation.RLA_OR_RRA, newVal, newLastBit);
+					res.ClockCycles = 8;
+					break;
+				case 0x18:
+					oldVal = registers.B;
+					bit0Val = (byte)(oldVal & 0x01);
+					newLastBit = (byte)( bit0Val == 0 ? 0 : 1);
+					newVal = (byte)((oldVal >> 1) + (registers.GetFlag(Flags.C) == 1 ? 0xFF : 0));
+					registers.B = newVal;
+					registers.SetFlagForUnsignedByteOperation (Operation.RLA_OR_RRA, newVal, newLastBit);
+					res.ClockCycles = 8;
+					break;
+				case 0x19:
+					oldVal = registers.C;
+					bit0Val = (byte)(oldVal & 0x01);
+					newLastBit = (byte)( bit0Val == 0 ? 0 : 1);
+					newVal = (byte)((oldVal >> 1) + (registers.GetFlag(Flags.C) == 1 ? 0xFF : 0));
+					registers.C = newVal;
+					registers.SetFlagForUnsignedByteOperation (Operation.RLA_OR_RRA, newVal, newLastBit);
+					res.ClockCycles = 8;
+					break;
+				case 0x1A:
+					oldVal = registers.D;
+					bit0Val = (byte)(oldVal & 0x01);
+					newLastBit = (byte)( bit0Val == 0 ? 0 : 1);
+					newVal = (byte)((oldVal >> 1) + (registers.GetFlag(Flags.C) == 1 ? 0xFF : 0));
+					registers.D = newVal;
+					registers.SetFlagForUnsignedByteOperation (Operation.RLA_OR_RRA, newVal, newLastBit);
+					res.ClockCycles = 8;
+					break;
+				case 0x1B:
+					oldVal = registers.E;
+					bit0Val = (byte)(oldVal & 0x01);
+					newLastBit = (byte)( bit0Val == 0 ? 0 : 1);
+					newVal = (byte)((oldVal >> 1) + (registers.GetFlag(Flags.C) == 1 ? 0xFF : 0));
+					registers.E = newVal;
+					registers.SetFlagForUnsignedByteOperation (Operation.RLA_OR_RRA, newVal, newLastBit);
+					res.ClockCycles = 8;
+					break;
+				case 0x1C:
+					oldVal = registers.H;
+					bit0Val = (byte)(oldVal & 0x01);
+					newLastBit = (byte)( bit0Val == 0 ? 0 : 1);
+					newVal = (byte)((oldVal >> 1) + (registers.GetFlag(Flags.C) == 1 ? 0xFF : 0));
+					registers.H = newVal;
+					registers.SetFlagForUnsignedByteOperation (Operation.RLA_OR_RRA, newVal, newLastBit);
+					res.ClockCycles = 8;
+					break;
+				case 0x1D:
+					oldVal = registers.L;
+					bit0Val = (byte)(oldVal & 0x01);
+					newLastBit = (byte)( bit0Val == 0 ? 0 : 1);
+					newVal = (byte)((oldVal >> 1) + (registers.GetFlag(Flags.C) == 1 ? 0xFF : 0));
+					registers.L = newVal;
+					registers.SetFlagForUnsignedByteOperation (Operation.RLA_OR_RRA, newVal, newLastBit);
+					res.ClockCycles = 8;
+					break;
+				case 0x1E:
+					oldVal = memoryAccess.ReadByteAtAddress (registers.HL);
+					bit0Val = (byte)(oldVal & 0x01);
+					newLastBit = (byte)(bit0Val == 0 ? 0 : 1);
+					newVal = (byte)((oldVal >> 1) + (registers.GetFlag (Flags.C) == 1 ? 0xFF : 0));
+					memoryAccess.WriteAtAddress (registers.HL, newVal);
+					registers.SetFlagForUnsignedByteOperation (Operation.RLA_OR_RRA, newVal, newLastBit);
+					res.ClockCycles = 8;
+					break;
+
+
+				default:
+					break;
+				}
+
+				if (res.ClockCycles > 0)
+					registers.GetPC ();
+
 
 				break;
 			}
