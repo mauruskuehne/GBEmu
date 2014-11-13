a1696f6209fdbde52938caf8f8c3e6d2a09c74db
Maurus Kühne <mauruskuehne@gmail.com>
2014-11-13 19:36:52 +0100

    lets do this again in swift!

---
+++ GBEmu/GBEmu/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GBEmu
+//
+//  Created by Maurus Kühne on 13.11.14.
+//  Copyright (c) 2014 Maurus Kühne. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(aNotification: NSNotification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(aNotification: NSNotification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ GBEmu/GBEmu/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GBEmu
+//
+//  Created by Maurus Kühne on 13.11.14.
+//  Copyright (c) 2014 Maurus Kühne. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: AnyObject? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
