41caa5ecac0e6f01b0c23e966235a60fddfc21de
Maurus Kühne <mauruskuehne@gmail.com>
2014-11-15 18:24:48 +0100

    ... github, why do you always forget one file?????

---
+++ GBEmu/GBEmu/LD.swift
@@ -0,0 +1,36 @@
+//
+//  LD.swift
+//  GBEmu
+//
+//  Created by Maurus Kühne on 15.11.14.
+//  Copyright (c) 2014 Maurus Kühne. All rights reserved.
+//
+
+import Foundation
+
+class LD : Instruction {
+  
+  let readLocation : ReadableDataLocation
+  let writeLocation : WriteableDataLocation
+  
+  override var description : String {
+    get {
+      return "LD \(writeLocation), \(readLocation)"
+    }
+  }
+  
+  init(readLocation : ReadableDataLocation, writeLocation : WriteableDataLocation) {
+    self.readLocation = readLocation
+    self.writeLocation = writeLocation
+    
+  }
+  
+  override func execute(context : EmulationContext) {
+    
+    let val = readLocation.read(context)
+    
+    writeLocation.write(val, context: context)
+  }
+}
+
+
