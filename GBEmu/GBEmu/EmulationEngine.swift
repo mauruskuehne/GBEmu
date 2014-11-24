c182d85a930caaee791dc85ce768ead6b509b0ba
Maurus Kühne <mauruskuehne@gmail.com>
2014-11-24 22:10:07 +0100

    ...

---
+++ GBEmu/GBEmu/EmulationEngine.swift
@@ -0,0 +1,44 @@
+//
+//  EmulationEngine.swift
+//  GBEmu
+//
+//  Created by Maurus Kühne on 15.11.14.
+//  Copyright (c) 2014 Maurus Kühne. All rights reserved.
+//
+
+import Foundation
+
+class EmulationEngine {
+  
+  let parser : OpcodeParser
+  var romData : NSData!
+  var registers : Registers!
+  var executionContext : ExecutionContext!
+  var memoryAccess : MemoryAccessor!
+  
+  init() {
+    parser = OpcodeParser()
+  }
+  
+  func loadRom(romData : NSData) {
+    self.romData = romData
+  }
+  
+  func setupExecution() {
+    self.registers = Registers()
+    self.memoryAccess = MemoryAccessor(rom: self.romData)
+    self.executionContext = ExecutionContext(registers: registers, memoryAccess : memoryAccess)
+  }
+  
+  func executeNextStep() {
+    
+    
+    var workingAddress = self.registers.PC
+    let firstOpcodeByte =  memoryAccess.readUInt8(workingAddress++)
+    
+    let instruction = parser.parseInstruction(firstOpcodeByte, fetchNextBytePredicate: {
+      return self.memoryAccess.readUInt8(workingAddress++)
+    })
+  }
+  
+}
